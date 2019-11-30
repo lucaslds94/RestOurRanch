306b83b648c0eed2b84c1ca55e8bc2af728dfa44
Lucas Lima <lucas.eng.lds@gmail.com>
2019-11-30 15:04:30 -0300

    Adicionar animacao nos links do menu

--- website/static/script.js
+++ website/static/script.js
@@ -1,9 +1,20 @@
 const navSlide = () => {
     const burger = document.querySelector('.menuIcon');
     const nav = document.querySelector('.menu');
-
+    const menuLinks = document.querySelectorAll('.menu a');
+    
     burger.addEventListener('click', () => {
+        // Toggle no burger
         nav.classList.toggle('navActive');
+
+        //Animação dos links
+        menuLinks.forEach((link, index) =>{
+            if(link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `menuFade 0.5s ease forwards ${index / 7+0.4}s`;    
+            }
+        });
     });
 }
 
