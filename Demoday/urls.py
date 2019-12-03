a5f6b8aacf1fd694dd69ecb4a4addcef4557953d
Lucas Lima <lucas.eng.lds@gmail.com>
2019-12-03 20:24:33 -0300

    Adicionar tela de login e estrutura no back

--- Demoday/urls.py
+++ Demoday/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
     path('register/', v.register, name = "register" ),
+    path('', include("django.contrib.auth.urls")),
 ]
