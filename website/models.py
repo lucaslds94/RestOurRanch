3b47b3960750c433a7285e986adcf76ce08ba4d9
Lucas Lima <lucas.eng.lds@gmail.com>
2019-12-09 17:06:13 -0300

    Limpar código

--- website/models.py
+++ website/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-# class InfoUsuario (models.Model):
-#     usuario = models.CharField(max_length = 100)
-#     email = models.EmailField(max_length = 100)
-#     senha = models.CharField(max_length = 100)
-
-#     def __str__(self):
-#         return self.usuario
