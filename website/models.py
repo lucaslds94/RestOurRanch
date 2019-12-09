d417ad0385ce05aa8877d7908167939f7f075a56
R2D2 <erikabellidoalarcon@gmail.com>
2019-12-09 17:48:31 -0300

    Adicionar redirecionamento htmls restaurantes

--- website/models.py
+++ website/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import numpy as np
 
 # Create your models here.
 
@@ -9,3 +10,28 @@ from django.db import models
 
 #     def __str__(self):
 #         return self.usuario
+
+class Restaurante(models.Model):
+    nome = models.CharField(max_length=200)
+    
+    def average_rating(self):
+        all_ratings = map(lambda x: x.rating, self.review_set.all())
+        return np.mean(all_ratings)
+        
+    def __unicode__(self):
+        return self.nome
+
+
+class Review(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+    restaurantes = models.ForeignKey(Restaurante, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('data publicada')
+    usuario = models.CharField(max_length=100)
+    comment = models.CharField(max_length=200)
+    rating = models.IntegerField(choices=RATING_CHOICES)
