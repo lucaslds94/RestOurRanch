1461b3d37ce3af23eca24b278143b31319c84ee0
R2D2 <erikabellidoalarcon@gmail.com>
2019-12-09 17:47:21 -0300

    adicionar admin register pra restaurantes e reviews

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from website.models import Restaurante, Review
 
 # Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ('restaurantes', 'rating', 'usuario', 'comment', 'pub_date')
+    list_filter = ['pub_date', 'usuario']
+    search_fields = ['comment']
+    
+admin.site.register(Restaurante)
+admin.site.register(Review, ReviewAdmin)
