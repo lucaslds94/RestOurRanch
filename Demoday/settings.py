06d51aca8189e7e382ebf119eba058df9de7f583
Lucas Lima <lucas.eng.lds@gmail.com>
2019-12-11 14:04:35 -0300

    Modificar cores e posicionamento do footer

--- Demoday/settings.py
+++ Demoday/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import django_heroku
+# import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -130,4 +130,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
